34449a3e5decc135f526d947a09bc7a83a8a70ca
Ankit Rewar <AnkitRewar11@users.noreply.github.com>
2026-03-06 15:24:47 +0530

    first look

---
+++ dstp_frontend/client/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import HomeHero from './HomeHero'
+import HomeWhoWeAre from './HomeWhoWeAre'
+import HomeExpertise from './HomeExpertise'
+
+export default function Home() {
+  return (
+    <div>
+      <HomeHero />
+      <HomeWhoWeAre />
+      <HomeExpertise />
+    </div>
+  )
+}
